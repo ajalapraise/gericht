a6eb21a48b9800ad3a44f627d7c3012cc1325692
ajalapraise <akintomiisin@gmail.com>
2023-11-02 23:24:32 +0100

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
+import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>getstarted | HomePage </title>
+      </Head>
+
+      <main className={`min-h-screen `}>
+
+      </main>
     </>
   )
 }
