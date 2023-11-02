a7058e67eeb2bea7e5d96e93e3c6ee3d6ed4e872
ajalapraise <akintomiisin@gmail.com>
2023-11-02 21:52:23 +0100

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import Layout from '@/components/Layout/Layout';
+import type { AppProps } from "next/app";
+import { Montserrat } from "next/font/google";
+import { Open_Sans } from "next/font/google";
+import Navbar from "@/components/NavBar/Navbar";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+const openSans = Open_Sans({
+  variable: '--openSans',
+  subsets: ["latin"],
+  display: "swap",
+  weight: "300",
+});
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <Layout>
+      <Navbar />
+      <main className={`text-[#AAA] ${openSans.className}`}>
+        <Component {...pageProps} />
+      </main>
+    </Layout>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "888px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontSize: {
+      base: "14px",
+      sm: "18px",
+      md: "20px",
+      lg: "24px",
+      xl: "32px",
+      "2xl": "40px",
+    },
+    colors: {
+
+      transparent: 'transparent',
+
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
