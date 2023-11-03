7d82a00c3f27134cf3e56f48d1851d9e2c2b88bc
ajalapraise <akintomiisin@gmail.com>
2023-11-03 12:18:18 +0100

    update

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <div
-            className={`flex min-h-[100vh] bg-[#0C0B08] text-[#AAA] px-16 flex-col justify-stretch items-stretch`}
+            className={`flex min-h-[100vh] bg-[#0C0B08] text-[#AAA] px-10 flex-col justify-stretch items-stretch`}
         >
             <main className={`grow`}>{children}</main>
         </div>
--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,42 +1,70 @@
 import React from 'react'
 import { cormorantUpright } from '@/styles/font'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const Navbar = () => {
+    const router = useRouter()
+
+    const isLinkActive = (linkPath: string) => {
+        return router.asPath === linkPath
+    }
+
     return (
-        <div className='flex fixed text-[#fff] w-full justify-between items-center py-8'>
-            <div className={`${cormorantUpright.className} w-1/5 text-xl`}>
-                <h1>Gerícht</h1>
+        <nav className='flex fixed text-[#fff] w-[92%] justify-between h-28 items-center py-8'>
+            <div className={`${cormorantUpright.className} flex  items-center h-full text-xl`}>
+                <Link href='/'>Gerícht</Link>
             </div>
-            <div className='flex w-1/3 justify-between'>
-                <ul className='flex w-full space-x-5 '>
-                    <li className=' flex items-center'>
-                        <Link href='#'>Home</Link>
-                    </li>
-                    <li className=' flex items-center'>
-                        <Link href='#'>Pages</Link>
-                    </li>
-                    <li className=' flex items-center'>
-                        <Link href='#'>Contact Us</Link>
-                    </li>
-                    <li className=' flex items-center'>
-                        <Link href='#'>Blog</Link>
-                    </li>
-                    <li className=' flex items-center'>
-                        <Link href='#'>Landing</Link>
-                    </li>
-                </ul>
-            </div>
-            <div className='flex w-1/3 justify-around items-center'>
-                <div className='w-1/3'>
-                    <Link href='#'>Log in / registration</Link>
-                </div>
-                <div className='bg-[white] w-[2px] h-10 rounded-lg'></div>
-                <div className='w-1/3'>
-                    <Link href='#'>book table</Link>
-                </div>
-            </div>
-        </div>
+            <ul className='flex items-center h-full gap-x-8'>
+                <li className=' flex items-center'>
+                    <Link
+                        href='/'
+                        className={` ${isLinkActive("/") ? "text-[#DCCA87]" : ""}`}
+                    > Home </Link>
+                </li>
+                <li className=' flex items-center'>
+                    <Link
+                        href='#'
+                        className={` ${isLinkActive("pages") ? "text-[#DCCA87]" : ""}`}
+                    >Pages</Link>
+                </li>
+                <li className=' flex items-center'>
+                    <Link
+                        href='#'
+                        className={` ${isLinkActive("contact") ? "text-[#DCCA87]" : ""}`}
+                    >Contact Us</Link>
+                </li>
+                <li className=' flex items-center'>
+                    <Link
+                        href='#'
+                        className={` ${isLinkActive("blog") ? "text-[#DCCA87]" : ""}`}
+                    >Blog</Link>
+                </li>
+                <li className=' flex items-center'>
+                    <Link
+                        href='#'
+                        className={` ${isLinkActive("landing") ? "text-[#DCCA87]" : ""}`}
+                    >Landing</Link>
+                </li>
+            </ul>
+            <ul className=' flex gap-x-10 h-full items-center justify-between'>
+                <li>
+                    <Link
+                        href='#'
+                        className={` ${isLinkActive("login") ? "text-[#DCCA87]" : ""}`}
+                    >Log in / registration</Link>
+                </li>
+                <li className='w-[1px] h-full  bg-[white]'>
+
+                </li>
+                <li>
+                    <Link
+                        href='#'
+                        className={` ${isLinkActive("book") ? "text-[#DCCA87]" : ""}`}
+                    >book table</Link>
+                </li>
+            </ul>
+        </nav >
     )
 }
 
