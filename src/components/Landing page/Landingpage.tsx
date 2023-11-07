62211e705ceeaa50622095f3242d0b0ae65f3ccd
ajalapraise <akintomiisin@gmail.com>
2023-11-07 14:13:53 +0100

    update

--- src/components/Landing page/Landingpage.tsx
+++ src/components/Landing page/Landingpage.tsx
@@ -8,26 +8,28 @@ import spoon from '../../../public/svgs/spoon.svg'
 
 export const Herosection = () => {
     return (
-        <div className='flex mt-40 justify-between items-center px-20'>
-            <div className='w-1/2 space-y-3'>
-                <div className='space-y-2'>
-                    <h1 className={`${cormorantUpright.className} text-sm font-extrabold`}> Chase the new Flavour </h1>
-                    <div className='w-[30px] relative h-[10px]'>
-                        <Image src={spoon} fill alt='' />
+        <div className='flex mt-40 justify-center items-center px-20'>
+            <div className='w-[90%] flex justify-between items-center'>
+                <div className='w-1/2 space-y-3'>
+                    <div className='space-y-2'>
+                        <h1 className={`${cormorantUpright.className} text-sm font-black`}> Chase the new Flavour </h1>
+                        <div className='ml-0 w-[30px] relative h-[10px]'>
+                            <Image src={spoon} fill alt='' />
+                        </div>
+                        <h1 className={`${cormorantUpright.className} text-[#DCCA87] text-2xl tracking-widest leading-tight w-[250px] `}>The Key To Fine Dining</h1>
+                        <p className={`text-[10px] font-semibold tracking-wide leading-loose w-[300px]`}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
                     </div>
-                    <h1 className={`${cormorantUpright.className} text-[#DCCA87] text-2xl leading-tight w-[200px] `}>The Key To Fine Dining</h1>
-                    <p className={`text-[10px] font-semibold w-[400px] tracking-wide leading-loose`}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+                    <div>
+                        <button>Explore Menu</button>
+                    </div>
+
                 </div>
-                <div>
-                    <button>Explore Menu</button>
-                </div>
-
-            </div>
-            <div className='w-1/2 flex items-center justify-center'>
-                <div className='w-[400px] relative h-[400px]'>
-                    <Image src={heroImg} fill alt='' />
+                <div className='w-1/2 flex items-center justify-center'>
+                    <div className='w-[400px] relative h-[400px]'>
+                        <Image src={heroImg} fill alt='' />
 
 
+                    </div>
                 </div>
             </div>
         </div>
